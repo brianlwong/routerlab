82bc360febeefe0adaa83f0649963263b529f42c
Brian Wong <brianwong1222@hotmail.com>
2016-04-19 23:03:06 -0500

    refactoring for clarity

--- RouterNode.java
+++ RouterNode.java
@@ -1,61 +1,61 @@
 import java.lang.String;
 
 public class RouterNode {
-    private int myID;
-    private int n;
-    private GuiTextArea myGUI;
-    private RouterSimulator sim;
+    private int myID, spaces;
     private int[] costs = new int[RouterSimulator.NUM_NODES];
-    private int[] via = new int[RouterSimulator.NUM_NODES];  // routing vector
+    private int[] rVector = new int[RouterSimulator.NUM_NODES];  // routing vector
     private int[][] table = new int[RouterSimulator.NUM_NODES][RouterSimulator.NUM_NODES];
+    private GuiTextArea GUI;
+    private RouterSimulator simulator;
     private F displayString = new F();
 
-  //--------------------------------------------------
-  public RouterNode(int ID, RouterSimulator sim, int[] costs) {
+  public RouterNode(int ID, RouterSimulator simulator, int[] costs) {
         myID = ID;
-        this.sim = sim;
-        myGUI =new GuiTextArea("  Router "+ ID + "  ");
+        this.simulator = simulator;
+        GUI = new GuiTextArea("  Router "+ ID + "  ");
         System.arraycopy(costs, 0, this.costs, 0, RouterSimulator.NUM_NODES);
 
-        int[] myDistVec = new int[RouterSimulator.NUM_NODES];
+        int[] distVec = new int[RouterSimulator.NUM_NODES];
 
         for(int i = 0; i < RouterSimulator.NUM_NODES; i++){
-            myDistVec[i] = table[i][myID];
+            distVec[i] = table[i][myID];
         }
-
 
         // sending distance vector to neighbors
         for(int i = 0; i < RouterSimulator.NUM_NODES; i++){
               if(costs[i] != RouterSimulator.INFINITY && costs[i] != 0){  // if i-th is a neighbor
-                  this.sendUpdate(new RouterPacket(myID, i, myDistVec));
+                  this.sendUpdate(new RouterPacket(myID, i, distVec));
               }
         }
-          initializeTable();
-          printGUIDistanceTable();
+        initializeTable();
+        printGUIDistanceTable();
   }
 
-  //--------------------------------------------------
   private void initializeTable(){
       for(int i = 0; i < RouterSimulator.NUM_NODES; i++)
       {
           table[i][myID] = costs[i];
           if(RouterSimulator.POISON){
-              via[i] = -1;
+              rVector[i] = -1;
           }
 
           if(RouterSimulator.POISON && costs[i] != RouterSimulator.INFINITY){
-              via[i] = i;
+              rVector[i] = i;
           }
 
           for(int j = 0; j < RouterSimulator.NUM_NODES; j++){
               if(i != j && j != myID){
+                  //if nodes are not themselves, initialize to 999
                   table[i][j] = RouterSimulator.INFINITY;
               }
           }
       }
   }
 
-  //--------------------------------------------------
+  private void sendUpdate(RouterPacket pkt) {
+        simulator.toLayer2(pkt);
+    }
+
   public void receiveUpdate(RouterPacket pkt) {
       //flag to indicate if there has been a change in a node
       boolean updatedFlag = false;
@@ -64,7 +64,6 @@ public class RouterNode {
       if( pkt.sourceid == pkt.destid){
           updatedFlag = true;
       }
-
 
       // updating distance vector in table for neighbor
       for(int i = 0; i < RouterSimulator.NUM_NODES; i++){
@@ -85,7 +84,7 @@ public class RouterNode {
                   if(table[i][j] + costs[j] < min ){
                       min = table[i][j] + costs[j];
                       if(RouterSimulator.POISON)
-                          via[i] = j;
+                          rVector[i] = j;
                   }
               }
           }
@@ -103,7 +102,7 @@ public class RouterNode {
           for(int i = 0; i < RouterSimulator.NUM_NODES; i++){
               if(costs[i] != RouterSimulator.INFINITY && costs[i] != 0){
                   for(int j = 0; j < RouterSimulator.NUM_NODES; j++){
-                      if(RouterSimulator.POISON && via[j] == i && via[j] != j)
+                      if(RouterSimulator.POISON && rVector[j] == i && rVector[j] != j)
                           myDistVec[j] = RouterSimulator.INFINITY;
                       else
                           myDistVec[j] = table[j][myID];
@@ -113,54 +112,8 @@ public class RouterNode {
           }
       }
   }
-  
 
-  //--------------------------------------------------
-  private void sendUpdate(RouterPacket pkt) {
-    sim.toLayer2(pkt);
-  }
-  
-
-  //--------------------------------------------------
-  public void printGUIDistanceTable() {
-        //formatting display on GUI window
-        n = space(RouterSimulator.NUM_NODES) + 4;
-	    myGUI.println("Current table for " + myID +
-			"  at time " + sim.getClocktime());
-        String dispText = "node | ";
-        for(int i = 0; i < RouterSimulator.NUM_NODES; i++){
-            if(costs[i] != RouterSimulator.INFINITY){
-                dispText += displayString.format("       " + i + " | ", n);
-            }
-        }
-
-
-        myGUI.println(dispText);
-
-        dispText = "";
-        for(int i = 0; i < RouterSimulator.NUM_NODES * n*2; i++){
-            dispText += '-';
-        }
-        myGUI.println(dispText);
-
-        for(int i = 0; i < RouterSimulator.NUM_NODES; i++){
-              dispText = displayString.format(i, n+2) + " | ";
-              for(int j = 0; j < RouterSimulator.NUM_NODES; j++){
-                    if(costs[j] != RouterSimulator.INFINITY){
-                        if(table[i][j] >= RouterSimulator.INFINITY){
-                          dispText += displayString.format("   " + RouterSimulator.INFINITY + " | ", n);
-                        }
-                        else{
-                          dispText += displayString.format("       " + table[i][j] + " |  ", n);
-                        }
-                    }
-              }
-              myGUI.println(dispText);
-        }
-  }
-
-  //--------------------------------------------------
-  public void updateLinkCost(int dest, int newCost) {
+  public void updateLinkCost(int dest, int newCost){
       costs[dest] = newCost;
 
       int[] myDistVec = new int[RouterSimulator.NUM_NODES];
@@ -178,15 +131,48 @@ public class RouterNode {
       System.out.println("Route to " + dest +" costs " + newCost);
   }
 
-    //--------------------------------------------------
-    private int space(int n){
-        int i = 0;
-        while(n % 10 > 0){
-            n /= 10;
-            i++;
-        }
-        return i;
-    }
+  public void printGUIDistanceTable() {
+      //formatting display on GUI window
+      spaces = space(RouterSimulator.NUM_NODES) + 4;
+	  GUI.println("Current table for " + myID + "  at time " + simulator.getClocktime());
+      String dispText = "node | ";
+      for(int i = 0; i < RouterSimulator.NUM_NODES; i++){
+          if(costs[i] != RouterSimulator.INFINITY){
+              dispText += displayString.format("       " + i + " | ", spaces);
+          }
+      }
 
+      GUI.println(dispText);
+
+      dispText = "";
+      for(int i = 0; i < RouterSimulator.NUM_NODES * spaces *2; i++){
+          dispText += '-';
+      }
+      GUI.println(dispText);
+
+      for(int i = 0; i < RouterSimulator.NUM_NODES; i++){
+            dispText = displayString.format(i, spaces +2) + " | ";
+            for(int j = 0; j < RouterSimulator.NUM_NODES; j++){
+                  if(costs[j] != RouterSimulator.INFINITY){
+                      if(table[i][j] >= RouterSimulator.INFINITY){
+                          dispText += displayString.format("   " + RouterSimulator.INFINITY + " | ", spaces);
+                      }
+                      else{
+                          dispText += displayString.format("       " + table[i][j] + " |  ", spaces);
+                      }
+                  }
+            }
+            GUI.println(dispText);
+      }
+  }
+
+  private int space(int i){
+      int j = 0;
+      while(i % 10 > 0){
+          j++;
+          i /= 10;
+      }
+      return j;
+  }
 }
 
